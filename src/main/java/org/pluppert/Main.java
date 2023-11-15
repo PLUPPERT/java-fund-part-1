4c377e5b837b3f6287c35575c04384cefebbfca5
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-15 08:55:45 +0100

    - Do exercise 3

--- src/main/java/org/pluppert/Main.java
+++ src/main/java/org/pluppert/Main.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*
+        Scanner scanner = new Scanner(System.in);
         // Exercise 1:
+        /*
         System.out.println("Hello");
         System.out.println("Christian!");
         */
 
         // Exercise 2:
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter year: ");
+        /* System.out.print("Enter year: ");
         int year = scanner.nextInt();
         boolean leapYear = (year % 4) == 0;
 
@@ -24,5 +23,22 @@ public class Main {
         } else {
             System.out.println(year + " is not a leap year.");
         }
+        */
+
+        // Exercise 3:
+        System.out.print("Write a number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Write a second number: ");
+        double num2 = scanner.nextDouble();
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplier = num1 * num2;
+        double divisor = num1 / num2;
+
+        System.out.println(num1 +" + " + num2 + " = " + addition);
+        System.out.println(num1 +" - " + num2 + " = " + subtraction);
+        System.out.println(num1 +" * " + num2 + " = " + multiplier);
+        System.out.println(num1 +" / " + num2 + " = " + divisor);
+
     }
 }
